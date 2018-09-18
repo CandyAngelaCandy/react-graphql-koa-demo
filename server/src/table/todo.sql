9c1380b2d2cb815680c9221db7bcc009a8abf4a9
yyhuang <yyhuang@thoughworks.com>
2018-09-18 10:30:30 +0800

    sequelize todo table'

---
+++ server/src/table/todo.sql
@@ -0,0 +1,10 @@
+ create table todos(
+    `id` int PRIMARY KEY AUTO_INCREMENT not null,
+    `text` varchar(100) not null,
+    `completed` BOOLEAN,
+     `editable` BOOLEAN,
+     `visible` BOOLEAN,
+     `deleted` BOOLEAN default false,
+    `time` date,
+    `userid` int
+ );
